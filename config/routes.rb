37b419ab4104814c25989de31361cdf8f4dd38da
pooja <pooj.kanchan@gmail.com>
2016-04-04 12:17:20 -0700

    Added home controller to test authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :views
   devise_for :installs
+  get 'home/hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
